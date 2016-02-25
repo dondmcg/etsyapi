e30995b395c238a2ddbd9dd0e9924287411e13b0
Donald McGrath <donaldmcgrath@Donalds-MBP.home>
2016-02-24 23:03:38 -0500

    refactored js

--- public/js/2.js
+++ public/js/2.js
@@ -1,22 +1,12 @@
-var EtsyAPIModule = (function () {
+// endpoint
+// https://openapi.etsy.com/v2/users/testusername.js?callback=handleStuff&api_key=pwsh318hwrljh22zabbl3rtp
+// key:
+// pwsh318hwrljh22zabbl3rtp
 
-    // endpoint
-    // https://openapi.etsy.com/v2/users/testusername.js?callback=EtsyAPIList.getData&api_key=pwsh318hwrljh22zabbl3rtp
-    // key:
-    // pwsh318hwrljh22zabbl3rtp
+var endPoint = 'https://openapi.etsy.com/v2/listings/active.js?callback=getData&&includes=Images:1&api_key=pwsh318hwrljh22zabbl3rtp';
 
-    // set up vars
-    var search = document.getElementById('search'),
-        searchBtn = document.getElementById('search-btn'),
-        bookmarkBtn = document.getElementById('bookmark-btn'),
-        filterBarLinks = document.getElementById('header_nav').getElementsByTagName('a'),
-        endPoint = 'https://openapi.etsy.com/v2/listings/active.js?callback=EtsyAPIModule.getData&&includes=Images:1&api_key=pwsh318hwrljh22zabbl3rtp',
-        offset = 0,
-        limit = 24,
-        resultSet = '&limit=' + limit + '&offset=' + offset;
 
-    // public get hash
-    var getHashParams = function() {
+    function getHashParams() {
 
         var hashParams = {};
         var e,
@@ -33,16 +23,21 @@ var EtsyAPIModule = (function () {
         return hashParams;
     };
 
-    // public delay method
-    var delay = (function () {
-        var timer = 0;
-        return function (callback, ms) {
-            clearTimeout(timer);
-            timer = setTimeout(callback, ms);
-        };
-    })();
+    // Check the query string for keywords
+    var hasKeywords = getHashParams().keywords,
+    filterSet = (hasKeywords) ? '&keywords=' + hasKeywords : '';
+    document.getElementById('search').value = (hasKeywords) ? hasKeywords : '';
 
-    var getScript = function(source, callback) {
+    // Set limit and offset
+    var offset = 0,
+    limit = 24,
+    resultSet = '&limit=' + limit + '&offset=' + offset;
+
+    // get the data
+    getScript(endPoint + resultSet + filterSet);
+
+
+    function getScript(source, callback) {
         document.getElementById("list_loader").className = "glyphicon glyphicon-refresh icon-spin";
         document.getElementById("list_loader_overlay").className = "";
         var script = document.createElement('script');
@@ -61,12 +56,13 @@ var EtsyAPIModule = (function () {
         };
 
         script.src = source;
-    };
+    }
 
     function getData(data) {
-        //console.log(data);
+        console.log(data);
         // set vars
         var imageDiv = document.getElementById("etsyImages"), html = '';
+        //console.log(data);
         // check that there is data
         if (data.count > 0) {
             // check if there is a result set then clear it
@@ -107,40 +103,43 @@ var EtsyAPIModule = (function () {
         setTimeout(function () {
             document.getElementById("list_loader_overlay").className = "hidden";
         }, 1000);
-    };
+    }
 
-    var clearActive = function(pagLinks) {
-        for (var i = 0; i < pagLinks.length; i++) {
-            pagLinks[i].parentNode.className = '';
+    function setPagination(count, offset) {
+        var numPages = Math.ceil(count / 24),
+            paginator = document.getElementById("paginator"),
+            totalPages = document.getElementById("total_pages"),
+            totalPagesTxt = '',
+            html = '',
+            i,
+            c,
+            thisPage = (offset < 1) ? 1 : offset / 24;
+
+        function getSet(offset) {
+            var page = offset / 24, set = 1;
+            /*if
+             1-10  = 1
+             11-20 = 2
+             21-30 = 3
+             31-40 = 4
+             41-50 = 5
+             */
+
+            for (var c = 1; c <= page; c++) {
+                if (c % 10 === 0) set = set + 1;
+                if (page === c) return set;
+            }
         }
-    };
 
-    var getSet = function(offset) {
-        var page = offset / 24, set = 1;
-        /*if
-         1-10  = 1
-         11-20 = 2
-         21-30 = 3
-         31-40 = 4
-         41-50 = 5
-         */
-        for (var i = 1; i <= page; i++) {
-            if (i % 10 === 0) set = set + 1;
-            if (page === i) return set;
-        }
-    };
-
-    function buildPagination(numPages, thisPage, set) {
-        //sample html
-        // <li><a href="#">1</a></li>
-        //<li class="active"><a href="#">2</a></li>
-        //<li><a href="#">3</a></li>
+        var set = (offset === '0') ? 1 : getSet();
+        // if offset is less than 240 or the pagination is only the first ten pages
+        // then hide the previous button otherwisse show it
         var prevClass = (offset < '240') ? 'hidden' : '';
-        // if the offset is the last set then hide the next button
+        // if the offset is the last set then remove the next button
         var nxtClass = (offset > ((numPages * 24) - 240 )) ? 'hidden' : '';
         // draw in the prev button
-        var html = '<li><a href="" id="prev_set" data-set="' + set + '" class="' + prevClass + '">«</a></li>';
-        for (var i = 1; i <= numPages; i++) {
+        html = '<li><a href="" id="prev_set" data-set="' + set + '" class="' + prevClass + '">«</a></li>';
+        for (i = 1; i <= numPages; i++) {
             var dataoffset = i * 24,
                 setStart = (set * 10) - 9,
                 setEnd = set * 10;
@@ -153,89 +152,76 @@ var EtsyAPIModule = (function () {
             }
         }
         //draw in the next button
-        html = html + '<li><a href="#" id="next_set" data-set="' + set + '" class="' + nxtClass + '">»</a></li>';
+        html = ( numPages > 10 ) ? html + '<li><a href="#" id="next_set" data-set="' + set + '" class="' + nxtClass + '">»</a></li>' : html;
 
-        return html;
-    };
+        //sample html
+        // <li><a href="#">1</a></li>
+        //<li class="active"><a href="#">2</a></li>
+        //<li><a href="#">3</a></li>
 
-    function prevNextHandler(element, pagLinks, numSet, pre_nxt) {
-        var dataset = element.getAttribute("data-set"),
-            endSet = (pre_nxt === 'prev')? (Number(dataset) - 1) * 10 : (Number(dataset) + 1) * 10,
-            startSet = (pre_nxt === 'prev')? ((dataset - 2) * 10) + 1 : (dataset * 10) + 1,
-            startPage = endSet - 9,
-            startPageBtn = document.getElementById('pp' + startPage);
-        startPageBtn.parentNode.className = "active";
-        for (var i = 0; i < pagLinks.length; i++) {
-            pagLinks[i].className = "";
-            var prevNxt = pagLinks[i].id;
-            if (i < startSet || i > endSet) {
-                pagLinks[i].className = "hidden";
-            }
-            if (prevNxt === 'prev_set' || prevNxt === 'next_set') {
-                pagLinks[i].className = "";
-            }
-        }
+        totalPagesTxt = 'Total Pages: ' + numPages;
+        totalPages.innerHTML = totalPagesTxt;
+        paginator.innerHTML = html;
 
-        var newDataSet = (pre_nxt === 'prev')? Number(dataset) - 1 : Number(dataset) + 1;
-        element.setAttribute("data-set", newDataSet);
-        var startEnd = (pre_nxt === 'prev')? 1 : numSet;
-        if (newDataSet === startEnd) element.className = 'visibility-none';
-        document.getElementById('prev_set').setAttribute("data-set", newDataSet);
-        offset = startPageBtn.getAttribute('data-offset');
-        resultSet = '&limit=' + limit + '&offset=' + offset;
-        getScript(endPoint + resultSet + filterSet);
-    };
-
-    function setPagination(count, offset) {
-        var numPages = Math.ceil(count / 24),
-            numSet = Math.ceil(numPages/10),
-            paginator = document.getElementById("paginator"),
-            totalPages = document.getElementById("total_pages"),
-            totalPagesTxt,
-            thisPage = (offset < 1) ? 1 : offset / 24,
-            set = (offset === '0') ? 1 : getSet(offset),
-            pagLinks = document.getElementById('paginator').getElementsByTagName("a");
-        if(pagLinks.length === 0) {
-            var html = buildPagination(numPages, thisPage, set);
-            paginator.innerHTML = html;
-        }
-
-        pagLinks = document.getElementById('paginator').getElementsByTagName("a");
+        var pagLinks = document.getElementById('paginator').getElementsByTagName("a");
         for (var i = 0; i < pagLinks.length; i++) {
             pagLinks[i].onclick = function (e) {
                 e.preventDefault();
                 if (this.id === 'prev_set') {
-                    prevNextHandler(this, pagLinks, numSet, 'prev');
+                    var dataset = this.getAttribute("data-offset");
+                    // TBD prev buttons not working no time
                     return;
-                };
+                }
+                ;
                 if (this.id === 'next_set') {
-                    prevNextHandler(this, pagLinks, numSet, 'nxt');
+                    var dataset = this.getAttribute("data-set"),
+                        endSet = (Number(dataset) + 1) * 10,
+                        startSet = (dataset * 10) + 1,
+                        startPage = endSet - 9,
+                        startPageBtn = document.getElementById('pp' + startPage);
+                    startPageBtn.parentNode.className = "active";
+                    for (var iter = 0; iter < pagLinks.length; iter++) {
+                        pagLinks[iter].className = "";
+                        var prevNxt = pagLinks[iter].id;
+                        if (iter < startSet || iter > endSet) {
+                            pagLinks[iter].className = "hidden";
+                        }
+                        if (prevNxt === 'prev_set' || prevNxt === 'next_set') {
+                            pagLinks[iter].className = "";
+                        }
+                    }
+                    var newDataSet = Number(dataset) + 1;
+                    this.setAttribute("data-set", newDataSet);
+                    document.getElementById('prev_set').setAttribute("data-set", newDataSet);
+                    offset = startPageBtn.getAttribute('data-offset');
+                    resultSet = '&limit=' + limit + '&offset=' + offset;
+                    getScript(endPoint + resultSet + filterSet);
                     return;
-                };
+                }
+                ;
                 offset = this.getAttribute("data-offset");
                 resultSet = '&limit=' + limit + '&offset=' + offset;
-                // clear current active
-                clearActive(pagLinks);
-                this.parentNode.className = 'active';
                 // get the data
                 getScript(endPoint + resultSet + filterSet);
             }
         }
+    }
 
-        // populate total pages text left of pagination
-        totalPagesTxt = 'Total Pages: ' + numPages;
-        totalPages.innerHTML = totalPagesTxt;
-    };
+    // reusable delay method
+    var delay = (function () {
+        var timer = 0;
+        return function (callback, ms) {
+            clearTimeout(timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
 
-    // Check the query string for keywords
-    var hasKeywords = getHashParams().keywords, filterSet = (hasKeywords) ? '&keywords=' + hasKeywords : '';
-    // populate the search box with hash values
-    document.getElementById('search').value = (hasKeywords) ? hasKeywords : '';
+    // event listeners
+    var search = document.getElementById('search'),
+    searchBtn = document.getElementById('search-btn'),
+    bookmarkBtn = document.getElementById('bookmark-btn'),
+    filterBarLinks = document.getElementById('header_nav').getElementsByTagName('a');
 
-    // get the data for list
-    getScript(endPoint + resultSet + filterSet);
-
-    // set up event listeners
     search.onkeyup = function () {
         delay(function () {
             var searchTerm = document.getElementById('search').value;
@@ -277,14 +263,4 @@ var EtsyAPIModule = (function () {
             getScript(endPoint + resultSet + filterSet);
             filterSet = '';
         };
-    };
-
-    return {
-        //publicMethods
-        getHashParams: getHashParams,
-        delay: delay,
-        getData: getData
-
-    };
-
-})();
+    }
